38b550af90078fb0cd3606109ced6f394ccb5b4f
Yujin <yujinpetercho@gmail.com>
2024-05-18 22:36:23 -0500

    initial push

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,69 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.subsystems.LedSubsystem;
+
+public class Robot extends TimedRobot {
+
+  private Spark leftMotor1 = new Spark(0);
+  private Spark leftMotor2 = new Spark(1);
+  private Spark rightMotor1 = new Spark(2);
+  private Spark rightMotor2 = new Spark(3);
+
+  private Joystick joy1 = new Joystick(0);
+
+  @Override
+  public void robotInit() {
+    //PLACEHOLDER led length
+    LedSubsystem led = new LedSubsystem();
+  }
+
+  @Override
+  public void robotPeriodic() {}
+
+  @Override
+  public void autonomousInit() {}
+
+  @Override
+  public void autonomousPeriodic() {}
+
+  @Override
+  public void teleopInit() {}
+
+  @Override
+  public void teleopPeriodic() {
+    //PLACEHOLDER axis value
+    double left = joy1.getRawAxis(1) * 0.6;
+    double right = joy1.getRawAxis(4) * 0.6;
+
+    leftMotor1.set(left);
+    leftMotor2.set(left);
+    rightMotor1.set(-right); //right opposite chassis
+    rightMotor2.set(-right);
+
+    //shuffle board log for speed
+    SmartDashboard.putNumber("Left Motor Speed", left);
+    SmartDashboard.putNumber("Right Motor Speed", right);
+  }
+
+  @Override
+  public void disabledInit() {}
+
+  @Override 
+  public void disabledPeriodic() {}
+
+  @Override
+  public void testInit() {}
+
+  @Override
+  public void testPeriodic() {}
+
+  @Override
+  public void simulationInit() {}
+
+  @Override
+  public void simulationPeriodic() {}
+}
---
+++ src/main/java/frc/robot/subsystems/LedSubsystem.java
@@ -0,0 +1,30 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+
+public class LedSubsystem {
+    //PLACEHOLDER pwm port
+    AddressableLED led;
+    AddressableLEDBuffer ledBuffer;
+    
+    public LedSubsystem() {
+        led = new AddressableLED(9);
+
+        //don't quite understand the point of a buffer
+        ledBuffer = new AddressableLEDBuffer(60);
+        led.setLength(ledBuffer.getLength());
+
+        led.setData(ledBuffer);
+        led.start();
+    }
+
+    public void setAllColor(int r, int g, int b) {
+        for (int i = 0; i < ledBuffer.getLength(); i++) {
+            ledBuffer.setRGB(i, r, g, b);
+        }
+    }
+    public void setColor(int led, int r, int g, int b) {
+        ledBuffer.setRGB(led, r, g, b);
+    }
+}
