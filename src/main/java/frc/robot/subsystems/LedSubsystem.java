94064d936330c97060481572732a7ba951953bde
Yujin <yujinpetercho@gmail.com>
2024-05-24 11:45:23 -0500

    color list created to maintain color values; addition of manual color changes using setColor

--- src/main/java/frc/robot/subsystems/LedSubsystem.java
+++ src/main/java/frc/robot/subsystems/LedSubsystem.java
@@ -2,12 +2,14 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import java.awt.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class LedSubsystem  extends SubsystemBase{
     //PLACEHOLDER pwm port
     AddressableLED led;
     AddressableLEDBuffer ledBuffer;
+    Color[] ledColors;
     
     public LedSubsystem() {
         led = new AddressableLED(9);
@@ -18,18 +20,24 @@ public class LedSubsystem  extends SubsystemBase{
 
         led.setData(ledBuffer);
         led.start();
+        ledColors = new Color[ledBuffer.getLength()];
+        setAllColor(0,0,0);
+        
     }
 
     @Override
     public void periodic() {
-        setAllColor(0, 0, 0);
+        for (int i = 0; i < ledColors.length; i++) {
+            Color c = ledColors[i];
+            ledBuffer.setRGB(i, c.getRed(), c.getGreen(), c.getBlue());
+        }
     }
     public void setAllColor(int r, int g, int b) {
-        for (int i = 0; i < ledBuffer.getLength(); i++) {
-            ledBuffer.setRGB(i, r, g, b);
+        for (int i = 0; i < ledColors.length; i++) {
+            setColor(i, r, g, b);
         }
     }
     public void setColor(int led, int r, int g, int b) {
-        ledBuffer.setRGB(led, r, g, b);
+        ledColors[led]= new Color(r,g,b);
     }
 }
